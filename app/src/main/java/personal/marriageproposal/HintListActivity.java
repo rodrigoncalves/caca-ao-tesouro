6d7cb47f2b8bec09cb2cd7e28abbe1c9e4835588
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2016-03-29 23:55:22 -0300

    Done button

--- app/src/main/java/personal/marriageproposal/HintListActivity.java
+++ app/src/main/java/personal/marriageproposal/HintListActivity.java
@@ -45,14 +45,14 @@ public class HintListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         View recyclerView = findViewById(R.id.hint_list);
         assert recyclerView != null;
