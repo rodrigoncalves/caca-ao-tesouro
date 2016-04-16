ac0eb34b7910cdd44560c020050fee154fc97a27
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2016-04-16 14:27:35 -0300

    Pistas até 3 e alterações no gradle

--- app/src/main/java/personal/marriageproposal/HintDetailActivity.java
+++ app/src/main/java/personal/marriageproposal/HintDetailActivity.java
@@ -1,12 +1,10 @@
 package personal.marriageproposal;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
